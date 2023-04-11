9d1d98fbffffd3fc8984a994cc1f2b2245d6dcca
dmateome-nttdata <130351288+dmateome-nttdata@users.noreply.github.com>
2023-04-11 11:22:36 +0200

    .

--- RetoInicial-Config/src/main/java/com/example/demo/RetoInicialConfigApplication.java
+++ RetoInicial-Config/src/main/java/com/example/demo/RetoInicialConfigApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
 public class RetoInicialConfigApplication {
 
 	public static void main(String[] args) {
